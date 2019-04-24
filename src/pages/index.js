0d6630b5a7fd3fa9ffd5e2f37914687e82a03ff5
Lewis Nelson <lewis.nelson@iohk.io>
2019-04-24 11:03:29 +0100

    Added netlify collection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
@@ -8,7 +8,22 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <StaticQuery
+      query={graphql`
+        query{
+          markdownRemark {
+            frontmatter {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <header>
+          <h1>{data.markdownRemark.frontmatter.title}</h1>
+        </header>
+      )}
+    />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
